2710ddf6bb3e2c08db060367adba40a59f9b4a67
Gabriel T. Nardy <gtnardy@gmail.com>
2022-02-22 10:58:41 -0300

    Improved the example

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 mkdir build
 cd build
 cmake ..
-cmake --build . --config Releas
+cmake --build . --config Release
 ```
--- src/example.cpp
+++ src/example.cpp
@@ -1,25 +1,28 @@
-#include "lua.h"
-#include "lauxlib.h"
-
-#ifndef LUAEXAMPLE_API
-#define LUAEXAMPLE_API
-#endif
-
-LUAEXAMPLE_API int luaopen_example (lua_State *L);
-
-static int test (lua_State *L) {
-    lua_pushliteral (L, "Hello World");
-    
-	return 1;
+// Needed includes to reference the Lua SDK
+extern "C" {
+    #include "lua.h"
+    #include "lauxlib.h"
 }
 
-LUAEXAMPLE_API int luaopen_example (lua_State *L) { 
-	struct luaL_Reg functions[] = {
-		{"test", test},
-		{NULL, NULL},
-	};
+// Definition of your custom functions
+int test(lua_State *L) {
+    // This will push 'Hello World' into the stack
+    // Which can be obtained as the returning value from Lua
+    lua_pushliteral(L, "Hello World");
 
-	luaL_newlib(L, functions);
+    // Amount of values returned (lua will get our string in the stack and pass as a return value)
+    return 1;
+}
 
-	return 1;
+// Definition of your custom library
+extern "C" int luaopen_example (lua_State *L) { 
+    // List of all custom functions
+    struct luaL_Reg functions[] = {
+        {"test", test},
+        {NULL, NULL},
+    };
+
+    luaL_newlib(L, functions);
+
+    return 1;
 }
